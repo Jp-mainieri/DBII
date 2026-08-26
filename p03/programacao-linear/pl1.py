057f7aad505199e0b003851e542ab1e3f43ffd9c
João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
2026-08-26 14:40:58 -0300

    starts the lab experiment on a04 and p03

---
+++ p03/programacao-linear/pl1.py
@@ -0,0 +1,8 @@
+# Uma marcenaria produz mesas (x) e cadeiras (y). Cada mesa dá lucro de R$ 8, cada cadeira dá lucro de R$ 6.
+#
+# Restrição de madeira: cada mesa usa 4 unidades de madeira, cada cadeira usa 2 unidades. Há 60 unidades de madeira disponíveis por semana.
+# Restrição de mão de obra: cada mesa leva 2 horas de trabalho, cada cadeira leva 2 horas. Há 40 horas disponíveis por semana.
+
+# Precisamos maximizar o lucro em função da quantidade de mesas (x) e cadeiras (y), com restrições de madeira e horas de mão de obra
+
+
